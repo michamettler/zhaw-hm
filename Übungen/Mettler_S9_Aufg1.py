11e0100d65754c606f6bc20cce19167e09a33ce7
mettlmi1 <mettlermicha@gmail.com>
2024-01-09 21:31:52 +0100

    changed structure

---
+++ Übungen/Mettler_S9_Aufg1.py
@@ -0,0 +1,11 @@
+# ----------------------------------------------------------------------------
+# HM1 SERIE 8 AUFGABE 2 / mettlmi1
+# ---------------------------------------------------------------------------
+
+import numpy as np
+
+# a)
+def S9_Aufg1_mettlmi(A):
+    return np.linalg.norm(np.linalg.inv(A), np.inf)
+
+
