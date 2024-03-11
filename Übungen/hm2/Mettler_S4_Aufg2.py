2cf62e6645ced53aadb18f109e3660c5ac9a37a7
mettlmi1 <mettlermicha@gmail.com>
2024-03-11 18:10:08 +0100

    finished series 4 task 2

---
+++ Übungen/hm2/Mettler_S4_Aufg2.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def lagrange_int(x, y, x_int):
+    n = np.size(x) - 1
+    L = np.ones(n+1)
+    
+    for i in range(n+1):
+        for j in range(n+1):
+            if i != j:
+                L[i] *= (x_int - x[j]) / (x[i] - x[j])
+                
+    y_int = 0
+    for i in range(n+1):
+        y_int += y[i] * L[i]
+    return y_int
+
+x = np.array([0., 2500., 5000., 10000.])
+y = np.array([1013., 747., 540., 226.])
+x_int = 3750.
+
+y_int = lagrange_int(x, y, x_int)
+print('y_int =', y_int)
