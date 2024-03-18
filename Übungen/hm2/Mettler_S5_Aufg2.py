1cf0c20f1bdbf1895bc828ec5b4c0480acba1fb3
mettlmi1 <mettlermicha@gmail.com>
2024-03-18 21:28:51 +0100

    finished series 5 task 2

---
+++ Übungen/hm2/Mettler_S5_Aufg2.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def spline_interpolation(x, y, xx):
+    x = np.array(x)
+    y = np.array(y)
+    xx = np.array(xx)
+    
+    n = np.size(x) - 1
+    
+    h = x[1:n+1] - x[0:n] # step 0
+    a = y[0:n] # step 1
+    
+    # step 2
+    c = np.zeros(n+1)
+    A = np.zeros((n-1, n-1))
+    for i in range(n-1):
+        A[i, i] = 2*(h[i] + h[i+1])
+    for i in range(0, n-2):
+        A[i, i+1] = h[i+1]
+        A[i+1, i] = h[i+1]
+    z = 3 * ((y[2:n+1] - y[1:n]) / h[1:n] - (y[1:n] - y[0:n-1]) / h[0:n-1])
+    c[1:n] = np.linalg.solve(A, z)
+    
+    # step 3
+    b = (y[1:n+1] - y[0:n]) / h - h / 3 * (c[1:n+1] + 2*c[0:n]) 
+    
+    # step 4
+    d = (c[1:n+1] - c[0:n]) / (3*h)
+    
+    # evaluate the spline
+    m = np.size(xx)
+    yy = np.zeros(m)
+    for j in range(m):
+        i = 0
+        while xx[j] > x[i+1]:
+            i = i + 1
+        t = (xx[j] - x[i])
+        yy[j] = np.polyval([d[i], c[i], b[i], a[i]], t)
+    
+    return yy, a, b, c, d
+
+x = np.array([4., 6., 8., 10.])
+y = np.array([6., 3., 9., 0.])
+
+xx = np.arange(4., 10., 0.1)
+yy, a, b, c, d = spline_interpolation(x, y, xx)
+print(a); print(b); print(c); print(d)
+
+plt.plot(x, y, 'o', label='data points')
+plt.plot(xx, yy, ' x', label='spline')
+plt.legend(['data points', 'spline'])
+plt.show()
+     
